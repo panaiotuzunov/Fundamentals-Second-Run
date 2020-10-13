10dbda378954929e5ecb0aaa2a8558d1d0638e38
Panaiot Uzunov <panaiot.uzunov@gmail.com>
2020-10-13 17:18:33 +0300

    Added exercises.

---
+++ Data Types and Variables - Lab/01. Centuries to Minutes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.Centuries_to_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int centuries = int.Parse(Console.ReadLine());
+            int years = centuries * 100;
+            int days = (int)(years * 365.2422);
+            int hours = days * 24;
+            int minutes = hours * 60;
+
+            Console.WriteLine(centuries);
+            Console.WriteLine(years);
+            Console.WriteLine(days);
+            Console.WriteLine(hours);
+            Console.WriteLine(minutes);
+        }
+    }
+}
---
+++ Data Types and Variables - Lab/02. Circle Area (Precision 12)/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _02.Circle_Area__Precision_12_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:f12}", Math.PI * r *r);
+        }
+    }
+}
